77010eb1d170f28cb8928f12aee34c568a014a60
Arttu <arl00023@laurea.fi>
2026-04-17 15:27:55 +0300

    First commit

---
+++ backend/models/Post.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true ,minLength: 3},
+    content: { type: String, required: true, trim: true, minLength: 10 },
+    author: { type: String, required: true, trim: true }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('Post', postSchema);
